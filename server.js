bf2d4515a67488f180a5173e7d15184624ae0925
iamaman012 <111675545+iamaman012@users.noreply.github.com>
2024-05-08 01:41:13 +0530

    Updated Build

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 const express = require("express");
 // const ACTIONS = require("./src/Actions");
 const app = express();
-
+const path = require('path')
 const http = require("http"); // it is inbuilt in node
 
 const { Server } = require("socket.io");
@@ -81,7 +81,10 @@ io.on("connection", (socket) => {
     socket.leave();
   });
 });
-
+app.use(express.static("./client/build"));
+app.get('*',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+})
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Listening in port ${PORT}`);
