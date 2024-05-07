a348949f2e94439cca2102cdc231d11df4bd844b
iamaman012 <111675545+iamaman012@users.noreply.github.com>
2024-05-08 01:50:49 +0530

    change order

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 const express = require("express");
 // const ACTIONS = require("./src/Actions");
 const app = express();
-const path = require('path')
+const path = require("path");
 const http = require("http"); // it is inbuilt in node
 
 const { Server } = require("socket.io");
@@ -15,6 +15,10 @@ const io = new Server(server, {
   },
 }); // crate an instance of server class of socket.io
 app.use(cors);
+app.use(express.static("./client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 const userSocketMap = {};
 function getAllConnectedClients(roomId) {
   return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
@@ -81,10 +85,7 @@ io.on("connection", (socket) => {
     socket.leave();
   });
 });
-app.use(express.static("./client/build"));
-app.get('*',(req,res)=>{
-  res.sendFile(path.resolve(__dirname,"client","build","index.html"));
-})
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Listening in port ${PORT}`);
